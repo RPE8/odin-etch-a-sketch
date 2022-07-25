a1790ebb023d0367d15fd89a7ac90e4f68d22f91
RPE <vitali.shuman96@gmail.com>
2022-07-25 19:33:03 +0300

    Add logic to dynamicly create canvas and initial handlers for touch event

--- script.js
+++ script.js
@@ -1,1 +1,58 @@
 console.log("test)=");
+const canvasContainer = document.getElementById("canvas-container");
+
+const canvas = document.createElement("canvas");
+canvas.height = canvasContainer.offsetHeight;
+canvas.width = canvasContainer.offsetWidth;
+canvasContainer.appendChild(canvas);
+const ctx = canvas.getContext("2d"),
+  ctx_rect = ctx.canvas.getBoundingClientRect();
+
+let touchStart = attachTouchStart(canvas);
+let touchEnd = attachTouchEnd(canvas);
+let toucheMove = attachTouchMove(canvas);
+
+function attachTouchStart(element) {
+  const touch = {
+    touches: null,
+  };
+
+  const handleTouchStart = (event) => {
+    console.log("TouchStart");
+    touch.touches = event.touches;
+  };
+
+  canvas.addEventListener("touchstart", handleTouchStart);
+
+  return touch;
+}
+
+function attachTouchEnd(element) {
+  const touch = {
+    touches: null,
+  };
+
+  const handleTouchEnd = (event) => {
+    console.log("TouchEnd");
+    touch.touches = event.touches;
+  };
+
+  canvas.addEventListener("touchend", handleTouchEnd);
+
+  return touch;
+}
+
+function attachTouchMove(element) {
+  const touch = {
+    touches: null,
+  };
+
+  const handleTouchMove = (event) => {
+    console.log("TouchMove");
+    touch.touches = event.touches;
+  };
+
+  canvas.addEventListener("touchmove", handleTouchMove);
+
+  return touch;
+}
