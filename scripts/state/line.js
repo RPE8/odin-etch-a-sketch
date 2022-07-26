4b90b20fef4fd4cef7c4c2b2102de9f716bd1f42
RPE <vitali.shuman96@gmail.com>
2022-07-26 16:43:33 +0300

    Add a class that represents Line

---
+++ scripts/state/line.js
@@ -0,0 +1,12 @@
+class Line {
+  constructor({ xStart, yStart, yEnd, xEnd, color, thickness }) {
+    this.xStart = xStart;
+    this.yStart = yStart;
+    this.yEnd = yEnd;
+    this.xEnd = xEnd;
+    this.color = color;
+    this.thickness = thickness;
+  }
+}
+
+export default Line;
